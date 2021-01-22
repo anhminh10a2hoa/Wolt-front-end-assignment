1ff43288a3feb308238ad49287a0de5628195688
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-01-22 22:45:21 +0200

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, Fragment } from 'react';
-import { decode } from "blurhash";
+import { Blurhash } from "react-blurhash";
 import { Section } from "./models/Section";
 import restaurants_data from "./data/discovery_page.json";
 import './App.css';
 import { Restaurant } from './models/Restaurant';
+import Navigation from "./components/Navigation";
 
 function App() {
   const [data, setData] = useState([] as any[]);
@@ -14,24 +15,34 @@ function App() {
       sections.push(restaurants_data.sections[section]);
     }
     setData(sections);
-    getImageData();
     console.log(sections);
   }, [])
 
-  const getImageData = () => {
-    const pixels = decode("LEHV6nWB2yk8pyo0adR*.7kCMdnj", 32, 32);
-    console.log(pixels);
-  };
-
   return (
     <div className="App">
+      <Navigation />
       {
         data.map((section: Section) => (
-          <Fragment>
-            <h1>{section.title}</h1>
-            {/* {section.restaurants.map((restaurant: Restaurant) => (
-            ))} */}
-          </Fragment>
+          <div className="section-container">
+            <h1 className="section-title">{section.title}</h1>
+            <div className="restaurant-container">
+              {section.restaurants.map((restaurant: Restaurant) => (
+                <div className="restaurant">
+                  <Blurhash
+                    hash={restaurant.blurhash}
+                    width={480}
+                    height={270}
+                    resolutionX={32}
+                    resolutionY={32}
+                    punch={1}
+                    className="img"
+                  />
+                  <p className="status">{restaurant.online ? "Online" : "Offline"}</p>
+                  <h2 className="name">{restaurant.name}</h2>
+                </div>
+              ))}
+            </div>
+          </div>
         ))
       }
     </div>
